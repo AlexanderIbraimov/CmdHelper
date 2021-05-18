1d48429ec642a3c16668f126f946b67bb335890e
ibraimovsasha <ibraimovsasha@gmail.com>
2021-05-18 23:38:31 +0600

    Created telegram bot

---
+++ Cmd/Cmd/Classes/TelegramBot.cs
@@ -0,0 +1,45 @@
+﻿using Cmd.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace Cmd.Classes
+{
+    public class TelegramBot
+    {
+        private ITelegramBotClient client;
+        
+        public Func<string, string> OnCommand;
+
+        public TelegramBot()
+        {
+            
+            client = new TelegramBotClient(AppSettings.TelegramToken);
+
+            client.OnMessage += OnMessage;
+            client.StartReceiving();
+        }
+
+        private async void OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                switch (e.Message.Type) 
+                {
+                    case MessageType.Text:
+                        var newMesssage = OnCommand?.Invoke(e.Message.Text);
+                        await client.SendTextMessageAsync(e.Message.Chat.Id, newMesssage);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
--- Cmd/Cmd/Program.cs
+++ Cmd/Cmd/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Cmd.Classes;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,44 @@ namespace Cmd
 {
     class Program
     {
+        static Program()
+        {
+            ServicePointManager.SecurityProtocol = ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
+
         static void Main(string[] args)
         {
+            var telegram = new TelegramBot();
+            telegram.OnCommand += OnCommand;
+
+            Console.ReadKey();
+        }
+
+        private static string OnCommand(string command)
+        {
+            return ExecuteCommandSync(command);
+        }
+
+        public static string ExecuteCommandSync(object command)
+        {
+            try
+            {
+                var procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
+
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                var proc = new Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                Console.WriteLine(result);
+                return string.IsNullOrEmpty(result) ? "Invalid command" : result;
+            }
+            catch (Exception ex)
+            {
+                return "Error";
+            }
         }
     }
 }
